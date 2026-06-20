6bbf1d5b243be579cabac02ca8fec4e8d73babf2
08021976-22022006 <poojanas.dev@gmail.com>
2026-06-20 23:30:24 +0530

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,11 @@ const { connectDB } = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
-
     await connectDB();
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
         console.log(`Server running on port ${PORT}`);
     });
-
 };
 
 startServer();
